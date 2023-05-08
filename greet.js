f4c2bda0b98ef31f7c4aa40621f94496e5b50f98
progress <leswifil@gmail.com>
2023-05-08 12:55:21 +0200

    greetings initial

--- greet.js
+++ greet.js
@@ -5,8 +5,17 @@ var greetButton = document.querySelector(".greetButton");
 //references to output name
 var helloName = document.querySelector(".helloName");
 
+function greet(){
+    var nameEntered = enteredName.value
+    var validateName = nameEntered.match(/^[A-Za-z]+$/)
+    if(validateName){
+        helloName.innerHTML = validateName
+    }else if(!validateName){
+        helloName.innerHTML = "Enter a valid name"
+    }
+}
+
 greetButton.addEventListener("click", function(){
-    helloName.innerHTML = enteredName.value
-    //remove name after clicking button
-    enteredName.value = '';
+    greet();
+    enteredName.value = ""
 });
