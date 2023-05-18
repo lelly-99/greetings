03c28341c48746c6355bb277f2692258faf91e95
progress <leswifil@gmail.com>
2023-05-18 14:18:17 +0200

    update

--- widget/greet.js
+++ widget/greet.js
@@ -1,6 +1,6 @@
 
 const enteredName = document.querySelector('.enteredName');
-const counter = document.querySelector('.counter');
+const counterElement = document.querySelector('.counter');
 const languageOptions = document.querySelectorAll('.language');
 const errorMessageElem = document.querySelector('.errorMessage');
 const greetButton = document.querySelector('.greetButton');
@@ -14,35 +14,42 @@ const greetFunction = greet();
 function getSelectedLanguage() {
     var selectedLanguage = '';
     languageOptions.forEach(function (radio) {
-      if (radio.checked) {
-        selectedLanguage = radio.value;
-      }
+        if (radio.checked) {
+            selectedLanguage = radio.value;
+        }
     });
     return selectedLanguage;
-  }
+}
 
 
 // Event listener for greet button click
 greetButton.addEventListener('click', function () {
     var name = enteredName.value;
     var language = getSelectedLanguage();
-    greetFunction.getName(name);
+
+    localStorage.setItem("theNames", JSON.stringify(greetFunction.getName(name)))
+
+    var counter = parseInt(localStorage.getItem("greetCounter")) || 0
+    
+
+
     greetFunction.getLanguage(language);
     var greeting = greetFunction.greetUser();
     var errorMessage = greetFunction.errorMessage();
-    var namesStored = localStorage.getItem("namesGreeted")
-    var names = namesStored ? JSON.parse(namesStored) : [];
 
     //greet user function if called
     if (greeting) {
+        
+        counterElement.innerHTML = counter
+        counter++
+        localStorage.setItem("greetCounter", counter.toString())
+
         enteredName.value = ""
         helloName.innerHTML = greeting;
-        counter.innerHTML = greetFunction.getCounter();
         errorMessageElem.innerHTML = ""
-        names.push(name);
-        localStorage.setItem("namesGreeted", JSON.stringify(name))
-    //error message function is called
-    }else if(errorMessage) {
+        
+        //error message function is calledS
+    } else if (errorMessage) {
         errorMessageElem.innerHTML = errorMessage;
         helloName.innerHTML = '';
     } else {
@@ -60,5 +67,5 @@ greetButton.addEventListener('click', function () {
 // Event listener for reset counter button click
 resetCounterButton.addEventListener('click', function () {
     const resetCount = greetFunction.resetCounter();
-    counter.innerHTML = resetCount;
+    counterElement.innerHTML = resetCount;
 });
