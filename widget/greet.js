1ccb7aa90c5ffb71170c19ff8b99c82292fe1804
progress <leswifil@gmail.com>
2023-05-19 10:08:28 +0200

    updated counter

--- widget/greet.js
+++ widget/greet.js
@@ -8,7 +8,7 @@ const helloName = document.querySelector('.helloName');
 const resetCounterButton = document.querySelector('.resetCounterButton');
 
 // Create a new greet instance
-const greetFunction =  greet();
+const greetFunction = greet();
 
 //check for the selected radio button
 function getSelectedLanguage() {
@@ -27,10 +27,9 @@ greetButton.addEventListener('click', function () {
     var name = enteredName.value;
     var language = getSelectedLanguage();
 
-    localStorage.setItem("theNames", JSON.stringify(greetFunction.getName(name)))
+    localStorage.getItem("theNames", JSON.stringify(greetFunction.getName(name)))
+    var counter = parseInt(localStorage.getItem("greetCounter")) || 1
 
-    var counter = parseInt(localStorage.getItem("greetCounter")) || 0
-    
 
 
     greetFunction.getLanguage(language);
@@ -39,15 +38,16 @@ greetButton.addEventListener('click', function () {
 
     //greet user function if called
     if (greeting) {
-        
+
         counterElement.innerHTML = counter
         counter++
         localStorage.setItem("greetCounter", counter.toString())
 
         enteredName.value = ""
-        helloName.innerHTML = greeting;
+        helloName.innerHTML = greeting
+        //localStorage.setItem("theNames", greeting);
         errorMessageElem.innerHTML = ""
-        
+
         //error message function is calledS
     } else if (errorMessage) {
         errorMessageElem.innerHTML = errorMessage;
@@ -68,4 +68,5 @@ greetButton.addEventListener('click', function () {
 resetCounterButton.addEventListener('click', function () {
     const resetCount = greetFunction.resetCounter();
     counterElement.innerHTML = resetCount;
+    localStorage.clear()
 });
